494ede28948b9a35243b52838014c23298ee5771
GeorgyPylyp <132196813+GeorgyPylyp@users.noreply.github.com>
2023-05-29 23:21:35 +0300

    Update main.c

--- code 4(2.2)/main.c
+++ code 4(2.2)/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main() {
- int i=-20;
  int is=0;
- int ie=10;
- while(i<=ie){
- if (i>0){
- is += i;}
- i++;}
+ int a, b;
+  printf("\n задайте значення a=");
+ scanf("%d",&a);
+  printf("\n задайте значення b=");
+ scanf("%d",&b);
+ while(a<=b){
+ if (a>0){
+ is += a;}
+ a++;}
  int sx=is*is;
  printf("Квадрат суми додатніх чисел у діапазоні [-20;10] дорівнює %d\n", sx);
   return 0;
